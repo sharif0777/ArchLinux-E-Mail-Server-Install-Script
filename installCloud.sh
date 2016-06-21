0efb95353486e3fc6f2a25d7ef4f05a4c600b491
mnosov89 <arclite89@gmail.com>
2016-06-21 22:00:41 +0300

    Add files via upload

--- installCloud.sh
+++ installCloud.sh
@@ -4,11 +4,16 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 yellow=`tput setaf 3`
 reset=`tput sgr0`
+newline=`echo $'\n.'`
+newline=${cr%.}
 
 function readValue {
 	read -p "$green$1$reset" $2
 }
 
+function readPassword {
+	read -s -p "$green$1$reset$newline" $2
+}
 function printText {
 	echo "$yellow$1$reset"
 }
@@ -100,210 +105,7 @@ chown root:root $pemFile
 #Install email server and web server
 printText "Installing postfix, dovecot and owncloud software and dependencies"
 
-pacman -Sy postfix dovecot nginx php php-fpm php-gd php-mcrypt php-intl php-apcu wget
-
-#OwnCloud configuration
-sed -i 's/;extension=gd.so/extension=gd.so/g' /etc/php/php.ini
-sed -i 's/;extension=iconv.so/extension=iconv.so/g' /etc/php/php.ini
-sed -i 's/;extension=xmlrpc.so/extension=xmlrpc.so/g' /etc/php/php.ini
-sed -i 's/;extension=zip.so/extension=zip.so/g' /etc/php/php.ini
-sed -i 's/;extension=bz2.so/extension=bz2.so/g' /etc/php/php.ini
-sed -i 's/;extension=curl.so/extension=curl.so/g' /etc/php/php.ini
-sed -i 's/;extension=intl.so/extension=intl.so/g' /etc/php/php.ini
-sed -i 's/;extension=mcrypt.so/extension=mcrypt.so/g' /etc/php/php.ini
-sed -i 's/;extension=pdo_mysql.so/extension=pdo_mysql.so/g' /etc/php/php.ini
-sed -i 's/;extension=mysqli.so/extension=mysqli.so/g' /etc/php/php.ini
-sed -i 's/;zend_extension=opcache.so/zend_extension=opcache.so/g' /etc/php/php.ini
-sed -i 's/;extension=apcu.so/extension=apcu.so/g' /etc/php/conf.d/apcu.ini
-  
-if [[ ! -z "$owncloudDataPath" ]]; then
-	echo "open_basedir = /srv/http/:/home/:/tmp/:/usr/share/pear/:/usr/share/webapps/:$owncloudDataPath" >> /etc/php/php.ini
-fi
-  
-readValue "Install MySQL database and configuration for ownCloud (y/n)?" choice
-case "$choice" in 
-  n|N ) printText "Type the connection information for the existing ownCloud database"
-		
-		readValue "Enter the MySQL database name to be used by owncloud (ex. cloud):" owncloudDatabase
-		readValue "Enter the MySQL username to be used by owncloud (ex. cloud):" owncloudUser
-		read -s -p "Type password for user $owncloudUser:" owncloudPassword
-
-  ;;
-  y|Y ) printText "Setting up MySQL database"
-		pacman -S mariadb
-		mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-		systemctl enable mysqld.service
-		systemctl start mysqld
-		printText "Initializing MySQL root user. Type root password:"
-		mysql_secure_installation
-		
-		readValue "Enter the MySQL database name to be used by owncloud (ex. cloud):" owncloudDatabase
-		readValue "Enter the MySQL username to be used by owncloud (ex. cloud):" owncloudUser
-		read -s -p "Type password for user $owncloudUser:" owncloudPassword
-
-		owncloudSql="CREATE DATABASE $owncloudDatabase;
-					CREATE DATABASE rainloop;
-					CREATE USER $owncloudUser@localhost;
-					SET PASSWORD FOR $owncloudUser@localhost= PASSWORD('$owncloudPassword');
-					GRANT ALL PRIVILEGES ON $owncloudUser.* TO $owncloudUser@localhost IDENTIFIED BY '$owncloudPassword';
-					FLUSH PRIVILEGES;"
-
-
-		printText "\nExecuting database script. Type root password:"
-		mysql -u root -p -e "$owncloudSql"
-	;;
-  * ) printText "Invalid answer";;
-esac
-
-
-#Nginx configuration
-printText "Configuring nginx"
-
-
-nginx_config="
-worker_processes  1;
-
-events {
-    worker_connections  1024;
-}
-
-http {
-    include       mime.types;
-    default_type  application/octet-stream;
-    sendfile        on;
-	
-	#Protect against Logjam attacks
-	ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
-	ssl_dhparam /etc/ssl/dhparams.pem;
-	ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-    ssl_prefer_server_ciphers on;
-	
-	server {
-		listen       80;
-		server_name  $domain;
-		return 301 https://\$server_name\$request_uri;  # enforce https
-	}
-	
-	server {
-		listen 443 ssl;
-		server_name $domain;
-		ssl_certificate $crtFile;
-		ssl_certificate_key $keyFile;
-		root /srv/http/$domain;
-	 
-		client_max_body_size 10G; # set max upload size
-		fastcgi_buffers 64 4K;
-	 
-		rewrite ^/caldav(.*)$ /remote.php/caldav\$1 redirect;
-		rewrite ^/carddav(.*)$ /remote.php/carddav\$1 redirect;
-		rewrite ^/webdav(.*)$ /remote.php/webdav\$1 redirect;
-	 
-		index index.php;
-		error_page 403 /core/templates/403.php;
-		error_page 404 /core/templates/404.php;
-	 
-		location = /robots.txt {
-			allow all;
-			log_not_found off;
-			access_log off;
-		}
-	 
-		location ~ ^/(data|config|\.ht|db_structure\.xml|README) {
-			deny all;
-		}
-	 
-		location / {
-			# The following 2 rules are only needed with webfinger
-			rewrite ^/.well-known/host-meta /public.php?service=host-meta last;
-			rewrite ^/.well-known/host-meta.json /public.php?service=host-meta-json last;
-	 
-			rewrite ^/.well-known/carddav /remote.php/carddav/ redirect;
-			rewrite ^/.well-known/caldav /remote.php/caldav/ redirect;
-	 
-			rewrite ^(/core/doc/[^\/]+/)$ \$1/index.html;
-	 
-			try_files \$uri \$uri/ index.php;
-		}
-	 
-		location ~ ^(.+?\.php)(/.*)?$ {
-			try_files \$1 =404;
-	 
-			include fastcgi_params;
-			fastcgi_param MOD_X_ACCEL_REDIRECT_ENABLED on;
-			fastcgi_param SCRIPT_FILENAME \$document_root\$1;
-			fastcgi_param PATH_INFO \$2;
-			fastcgi_param HTTPS on;
-			fastcgi_pass unix:/run/php-fpm/php-fpm.sock;
-		}
-	 
-		location ~ ^$owncloudDataPath {
-			internal;
-			root /;
-		}
-	 
-		location ~ ^/tmp/oc-noclean/.+$ {
-			internal;
-			root /;
-		}
-	 
-		# Optional: set long EXPIRES header on static assets
-		location ~* ^.+\.(jpg|jpeg|gif|bmp|ico|png|css|js|swf)$ {
-			expires 30d;
-			# Optional: Don't log access to assets
-			access_log off;
-		}
-	}
-}"
-
-echo "$nginx_config" > /etc/nginx/nginx.conf
-
-printText "Downloading and installing owncloud"
-
-wget https://download.owncloud.org/community/owncloud-9.0.2.tar.bz2
-tar xfj owncloud-9.0.2.tar.bz2 -C /srv/http
-rm owncloud-9.0.2.tar.bz2
-mv /srv/http/owncloud /srv/http/$domain
-
-#Overwriting password hash generation for owncloud
-hasher_php="<?php
-namespace OC\Security;
-
-use OCP\IConfig;
-use OCP\Security\IHasher;
-
-class Hasher implements IHasher {
-	
-	private \$config;
-	private \$options = array();
-	private \$legacySalt = null;
-	private \$currentVersion = 1;
-
-	function __construct(IConfig \$config) {
-		\$this->config = \$config;
-
-		\$hashingCost = \$this->config->getSystemValue('hashingCost', null);
-		if(!is_null(\$hashingCost)) {
-			\$this->options['cost'] = \$hashingCost;
-		}
-	}
-
-	public function hash(\$message) {
-		return exec('doveadm pw -p '.\$message);
-	}
-	
-	public function verify(\$message, \$hash, &\$newHash = null) {
-		return (\$this->hash(\$message) == \$hash ? true : false);
-	}
-}
-"
-
-echo "$hasher_php" > /srv/http/$domain/lib/private/security/hasher.php
-
-#Setting proper permissions
-chgrp -R http /srv/http/$domain
-chmod -R 770 /srv/http/$domain
-mkdir $owncloudDataPath
-chown http:http $owncloudDataPath
+pacman -Sy postfix dovecot nginx php php-fpm php-gd php-mcrypt php-intl php-apcu wget sudo unzip
 
 
 #Postfix configuration
@@ -490,6 +292,239 @@ password_query = SELECT concat(uid,'@$domain') as user, password FROM oc_users W
 
 echo "$virtual_users_db" > /etc/dovecot/virtual-users-db
 chmod 600 /etc/dovecot/virtual-users-db
+
+
+#OwnCloud configuration
+sed -i 's/;extension=gd.so/extension=gd.so/g' /etc/php/php.ini
+sed -i 's/;extension=iconv.so/extension=iconv.so/g' /etc/php/php.ini
+sed -i 's/;extension=xmlrpc.so/extension=xmlrpc.so/g' /etc/php/php.ini
+sed -i 's/;extension=zip.so/extension=zip.so/g' /etc/php/php.ini
+sed -i 's/;extension=bz2.so/extension=bz2.so/g' /etc/php/php.ini
+sed -i 's/;extension=curl.so/extension=curl.so/g' /etc/php/php.ini
+sed -i 's/;extension=intl.so/extension=intl.so/g' /etc/php/php.ini
+sed -i 's/;extension=mcrypt.so/extension=mcrypt.so/g' /etc/php/php.ini
+sed -i 's/;extension=pdo_mysql.so/extension=pdo_mysql.so/g' /etc/php/php.ini
+sed -i 's/;extension=mysqli.so/extension=mysqli.so/g' /etc/php/php.ini
+sed -i 's/;zend_extension=opcache.so/zend_extension=opcache.so/g' /etc/php/php.ini
+sed -i 's/;extension=apcu.so/extension=apcu.so/g' /etc/php/conf.d/apcu.ini
+  
+  
+readValue "Install MySQL database and configuration for ownCloud (y/n)?" choice
+case "$choice" in 
+  n|N ) printText "Type the connection information for the existing ownCloud database"
+		
+		readValue "Enter the MySQL database name to be used by owncloud (ex. cloud):" owncloudDatabase
+		readValue "Enter the MySQL username to be used by owncloud (ex. cloud):" owncloudUser
+		readPassword "Type password for user $owncloudUser:" owncloudPassword
+
+  ;;
+  y|Y ) printText "Setting up MySQL database"
+		pacman -S mariadb
+		mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+		systemctl enable mysqld.service
+		systemctl start mysqld
+		printText "Initializing MySQL root user. Type root password:"
+		mysql_secure_installation
+		
+		readValue "Enter the MySQL database name to be used by owncloud (ex. cloud):" owncloudDatabase
+		readValue "Enter the MySQL username to be used by owncloud (ex. cloud):" owncloudUser
+		readPassword "Type password for user $owncloudUser:" owncloudPassword
+
+		owncloudSql="CREATE DATABASE $owncloudDatabase;
+					CREATE DATABASE rainloop;
+					CREATE USER $owncloudUser@localhost;
+					SET PASSWORD FOR $owncloudUser@localhost= PASSWORD('$owncloudPassword');
+					GRANT ALL PRIVILEGES ON $owncloudUser.* TO $owncloudUser@localhost IDENTIFIED BY '$owncloudPassword';
+					FLUSH PRIVILEGES;"
+
+
+		printText "\nExecuting database script. Type root password:"
+		mysql -u root -p -e "$owncloudSql"
+	;;
+  * ) printText "Invalid answer";;
+esac
+
+
+#Nginx configuration
+printText "Configuring nginx"
+
+
+nginx_config="
+worker_processes  1;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+    sendfile        on;
+	
+	#Protect against Logjam attacks
+	ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
+	ssl_dhparam /etc/ssl/dhparams.pem;
+	ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_prefer_server_ciphers on;
+	
+	server {
+		listen       80;
+		server_name  $domain;
+		return 301 https://\$server_name\$request_uri;  # enforce https
+	}
+	
+	server {
+		listen 443 ssl;
+		server_name $domain;
+		ssl_certificate $crtFile;
+		ssl_certificate_key $keyFile;
+		root /srv/http/$domain;
+	 
+		client_max_body_size 10G; # set max upload size
+		fastcgi_buffers 64 4K;
+	 
+		rewrite ^/caldav(.*)$ /remote.php/caldav\$1 redirect;
+		rewrite ^/carddav(.*)$ /remote.php/carddav\$1 redirect;
+		rewrite ^/webdav(.*)$ /remote.php/webdav\$1 redirect;
+	 
+		index index.php;
+		error_page 403 /core/templates/403.php;
+		error_page 404 /core/templates/404.php;
+	 
+		location = /robots.txt {
+			allow all;
+			log_not_found off;
+			access_log off;
+		}
+	 
+		location ~ ^/(data|config|\.ht|db_structure\.xml|README) {
+			deny all;
+		}
+	 
+		location / {
+			# The following 2 rules are only needed with webfinger
+			rewrite ^/.well-known/host-meta /public.php?service=host-meta last;
+			rewrite ^/.well-known/host-meta.json /public.php?service=host-meta-json last;
+	 
+			rewrite ^/.well-known/carddav /remote.php/carddav/ redirect;
+			rewrite ^/.well-known/caldav /remote.php/caldav/ redirect;
+	 
+			rewrite ^(/core/doc/[^\/]+/)$ \$1/index.html;
+	 
+			try_files \$uri \$uri/ index.php;
+		}
+	 
+		location ~ ^(.+?\.php)(/.*)?$ {
+			try_files \$1 =404;
+	 
+			include fastcgi_params;
+			fastcgi_param MOD_X_ACCEL_REDIRECT_ENABLED on;
+			fastcgi_param SCRIPT_FILENAME \$document_root\$1;
+			fastcgi_param PATH_INFO \$2;
+			fastcgi_param HTTPS on;
+			fastcgi_pass unix:/run/php-fpm/php-fpm.sock;
+		}
+	 
+		location ~ ^$owncloudDataPath {
+			internal;
+			root /;
+		}
+	 
+		location ~ ^/tmp/oc-noclean/.+$ {
+			internal;
+			root /;
+		}
+	 
+		# Optional: set long EXPIRES header on static assets
+		location ~* ^.+\.(jpg|jpeg|gif|bmp|ico|png|css|js|swf)$ {
+			expires 30d;
+			# Optional: Don't log access to assets
+			access_log off;
+		}
+	}
+}"
+
+echo "$nginx_config" > /etc/nginx/nginx.conf
+
+printText "Downloading and installing owncloud"
+
+wget https://download.owncloud.org/community/owncloud-9.0.2.tar.bz2
+tar xfj owncloud-9.0.2.tar.bz2 -C /srv/http
+rm owncloud-9.0.2.tar.bz2
+mv /srv/http/owncloud /srv/http/$domain
+
+#Overwriting password hash generation for owncloud
+hasher_php="<?php
+namespace OC\Security;
+
+use OCP\IConfig;
+use OCP\Security\IHasher;
+
+class Hasher implements IHasher {
+	
+	private \$config;
+	private \$options = array();
+	private \$legacySalt = null;
+	private \$currentVersion = 1;
+
+	function __construct(IConfig \$config) {
+		\$this->config = \$config;
+
+		\$hashingCost = \$this->config->getSystemValue('hashingCost', null);
+		if(!is_null(\$hashingCost)) {
+			\$this->options['cost'] = \$hashingCost;
+		}
+	}
+
+	public function hash(\$message) {
+		return exec('doveadm pw -p '.\$message);
+	}
+	
+	public function verify(\$message, \$hash, &\$newHash = null) {
+		return (\$this->hash(\$message) == \$hash ? true : false);
+	}
+}
+"
+
+echo "$hasher_php" > /srv/http/$domain/lib/private/security/hasher.php
+
+#Setting proper permissions
+chgrp -R http /srv/http/$domain
+chmod -R 770 /srv/http/$domain
+mkdir $owncloudDataPath
+chown http:http $owncloudDataPath
+
+
+#Init owncloud
+printText "Initializing ownCloud"
+readValue "Type admin username for owncloud:" adminUser
+readPassword "Type admin password for owncloud:" adminPassword	
+
+sudo -u http php /srv/http/$domain/occ  maintenance:install --database "mysql" --database-name "$owncloudDatabase"  --database-user "$owncloudUser" --database-pass "$owncloudUser" --admin-user "$adminUser" --admin-pass "$adminPassword" --data-dir "$owncloudDataPath"
+
+sed -i "s/),/   1 => '$domain',\n  ),/g" /srv/http/$domain/config/config.php
+sed -i "s/);/  'memcache.local' => '\\\OC\\\Memcache\\\APCu',\n);/g" /srv/http/$domain/config/config.php
+sed -i "s/);/  'appstore.experimental.enabled' => true,\n);/g" /srv/http/$domain/config/config.php
+sed -i "s/);/  'defaultapp' => 'rainloop',\n);/g" /srv/http/$domain/config/config.php
+  
+
+printText "Installing Contacts app for ownCloud"
+wget https://github.com/owncloud/contacts/releases/download/v1.3.1.0/contacts.tar.gz
+tar xfz contacts.tar.gz -C /srv/http/$domain/apps
+rm contacts.tar.gz
+sudo -u http /srv/http/$domain/occ app:enable contacts
+
+printText "Installing Calendar app for ownCloud"
+wget https://github.com/owncloud/calendar/releases/download/v1.2.2/calendar.tar.gz
+tar xfz calendar.tar.gz -C /srv/http/$domain/apps
+rm calendar.tar.gz
+sudo -u http /srv/http/$domain/occ app:enable calendar
+
+printText "Installing RainLoop Webmail app for ownCloud"
+wget http://repository.rainloop.net/v2/other/owncloud/rainloop.zip
+unzip -qq -o rainloop.zip -d /srv/http/$domain/apps
+rm rainloop.zip
+sudo -u http /srv/http/$domain/occ app:enable rainloop
 
 
 printText "Starting postfix, dovecot and nginx web server"
